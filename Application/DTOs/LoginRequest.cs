58070bec0f338cde9569246307c9a962c7e83486
Arrecis <arrecis059@gmail.com>
2026-05-23 17:49:59 -0600

    Fix => Validacion en DTO para estructura de peticion

--- Application/DTOs/LoginRequest.cs
+++ Application/DTOs/LoginRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace CobrosAutomaticosApi.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CobrosAutomaticosApi.Application.DTOs
 {
     public class LoginRequest
     {
+        [Required]
         public string UserName { get; init; } = string.Empty;
-
+        
+        [Required]
         public string Password { get; init; } = string.Empty;
     }
 }
