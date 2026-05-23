2689603af573b20e852827159f1ffe38a19e4b12
Arrecis <arrecis059@gmail.com>
2026-05-23 14:00:53 -0600

    Feature => Se agrega DTO(BaseResponse) para luego heredar esos campos en todas las respuestas

---
+++ Application/DTOs/BaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CobrosAutomaticosApi.Application.DTOs
+{
+    public record BaseResponse
+    {
+        public string StatusCode { get; init; } = string.Empty;
+
+        public string Message { get; init; } = string.Empty;
+    }
+}
