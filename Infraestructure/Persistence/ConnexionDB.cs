4def2b11754a7a7c13e0361e5a5f5e3248d9ca0f
Arrecis <arrecis059@gmail.com>
2026-05-23 15:14:04 -0600

    Feature => Se agrega funcionalidad para obtener cadena de conexión y crear la instancia a la conexión

---
+++ Infraestructure/Persistence/ConnexionDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CobrosAutomaticosApi.Infraestructure.Persistence
+{
+    public class ConnexionDB
+    {
+        private readonly string _connectionString;        
+
+        public ConnexionDB(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Local");
+        }
+
+        public async Task<SqlConnection> GetConnectionAsync()
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+                return connection;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error al conectar a la base de datos.");
+                throw;
+            }
+
+        }
+
+    }
+}
