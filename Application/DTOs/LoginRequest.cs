398231ab63a0984e3e3cb4f5a94a245783cfee93
Arrecis <arrecis059@gmail.com>
2026-05-23 14:28:43 -0600

    Feature => Se agregan DTOs para el request y response esperado en el LogIn

---
+++ Application/DTOs/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CobrosAutomaticosApi.Application.DTOs
+{
+    public class LoginRequest
+    {
+        public string UserName { get; init; } = string.Empty;
+
+        public string Password { get; init; } = string.Empty;
+    }
+}
---
+++ Application/DTOs/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CobrosAutomaticosApi.Application.DTOs
+{
+    public record LoginResponse : BaseResponse
+    {
+        public string UserName { get; init; } = string.Empty;
+
+        public string Token { get; init; } = string.Empty;
+
+    }
+}
