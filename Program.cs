e4c4f384cc57ce100bb0c7744f237105cecb4af6
Arrecis <arrecis059@gmail.com>
2026-05-23 17:48:14 -0600

    Feature => Registrar conexion, servicios y repositorios

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using CobrosAutomaticosApi.Application.Interfaces;
+using CobrosAutomaticosApi.Application.Services;
+using CobrosAutomaticosApi.Infraestructure.Persistence;
+using CobrosAutomaticosApi.Infraestructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Connection
+builder.Services.AddSingleton<ConnexionDB>();
+
+// Repositories
+builder.Services.AddScoped<AuthenticationRepository>();
+
+// Services
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
